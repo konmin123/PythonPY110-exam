cc63489c614e3e3116a1bcfbc0bdf98bd5a2fc9b
konmin123 <MindlinKS@yandex.ru>
2021-12-16 14:45:52 +0300

    + анатация типов, меняется номер книги.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 import json
+
 from faker import Faker
 
 from conf import MODEL
@@ -7,68 +8,75 @@ from conf import MODEL
 
 faker = Faker()
 
+list_for_title = []
+
+iter_for_title = iter(list_for_title)
+
 
 def main():
-    for_write = next(gen_list(1))
-    with open("output.txt", "w", encoding="utf-8") as f:
-        f.write(json.dumps(for_write, indent=4, ensure_ascii=False))
-        
-
-def title():
     with open("books.txt", "r", encoding="utf-8") as f:
         for line in f:
-            yield line.strip()
+            list_for_title.append(line.strip())
+    for_write = next(gen_list())
+    with open("output.txt", "w", encoding="utf-8") as f:
+        f.write(json.dumps(for_write, indent=4, ensure_ascii=False))
 
 
-def year():
+def title() -> str:
+    return next(iter_for_title)
+
+
+def year() -> int:
     year_in = random.randint(1800, 2021)
-    yield year_in
+    return year_in
 
 
-def pages():
+def pages() -> int:
     pages_in = random.randint(1, 300)
-    yield pages_in
+    return pages_in
 
 
-def isbn13():
+def isbn13() -> str:
     isbn13_in = faker.isbn13()
-    yield isbn13_in
+    return isbn13_in
 
 
-def rating():
+def rating() -> float:
     rating_in = random.uniform(0.0, 5.0)
-    yield rating_in
+    return rating_in
 
 
-def price():
+def price() -> float:
     price_in = random.uniform(5.0, 1000.0)
-    yield round(price_in)
+    return round(price_in, 2)
 
 
-def author():
-    author_in = faker.name()
-    yield author_in
+def author() -> str:
+    author_ = faker.name()
+    return author_
 
 
-def gen_list():
+def gen_list() -> list:
     list_gen = []
     book_generator = book_gen(1)
     for i in range(100):
         list_gen.append(next(book_generator))
-        
+
     yield list_gen
-    
-    
-def book_gen(pk=1):
+
+
+def book_gen(pk=1) -> dict:
     while True:
         book_dict = {
-            "model": MODEL, 
-            "pk": pk, 
-            "Title": next(title()), 
-            "year": next(year()),  
-            "pages": next(pages()), 
-            "isbn13": next(isbn13()), 
-            "rating": next(rating())
+            "model": MODEL,
+            "pk": pk,
+            "Title": title(),
+            "year": year(),
+            "pages": pages(),
+            "price": price(),
+            "isbn13": isbn13(),
+            "authors": author()+", "+author(),
+            "rating": rating()
         }
         yield book_dict
         pk += 1
@@ -76,4 +84,3 @@ def book_gen(pk=1):
 
 if __name__ == "__main__":
     main()
-
